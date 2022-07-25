691a88f7ae9b0cc13cc656a357d9d482890e8cb9
spellcry <sangiothebest@hotmail.com>
2022-07-25 22:17:54 +0200

    refactor dadi

--- dadi/js/scripts.js
+++ dadi/js/scripts.js
@@ -1,12 +1,37 @@
 const min = Math.ceil(1);
 const max = Math.floor(6);
-const computer = Math.floor(Math.random() * ( max - min + 1 ) + min);
-const player = Math.floor(Math.random() * ( max - min + 1 ) + min);
 
-if ( computer > player ) {
-    console.log(`Ha vinto il computer con un punteggio di ${computer} a ${player}`);
-} else if ( computer < player ) {
-    console.log(`Ha vinto il giocatore con un punteggio di ${player} a ${computer}`);
+const chiVinceElement = document.querySelector('.chi-vince');
+
+let playerResultElement = null, computerResultElement = null;
+
+if ( playerResultElement == null && computerResultElement == null ) {
+    playerResultElement = document.createElement('p');
+    playerResultElement.classList.add('player__result');
+    playerResultElement.classList.add('result');
+    computerResultElement = document.createElement('p');
+    computerResultElement.classList.add('computer__result');
+    computerResultElement.classList.add('result');
 } else {
-    console.log(`C'è stato un pareggio con un punteggio di ${player} a ${computer}`);
+    playerResultElement = document.querySelector('.player__result');
+    computerResultElement = document.querySelector('.computer__result');
 }
+
+const computerElement = document.querySelector('.results .computer');
+computerElement.append(computerResultElement);
+const playerElement = document.querySelector('.results .player');
+playerElement.append(playerResultElement);
+
+const giocaElement = document.addEventListener('click', function(){
+    const computer = Math.floor(Math.random() * ( max - min + 1 ) + min);
+    const player = Math.floor(Math.random() * ( max - min + 1 ) + min);
+    computerResultElement.innerHTML = computer;
+    playerResultElement.innerHTML = player;
+    if ( computer > player ) {
+        chiVinceElement.innerHTML = 'Hai perso!';
+    } else if ( computer < player ) {
+        chiVinceElement.innerHTML = 'Hai vinto!';
+    } else {
+        chiVinceElement.innerHTML = 'Pareggio!';
+    }
+});
