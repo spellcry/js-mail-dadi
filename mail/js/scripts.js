1ba1449cb5d48ce496ea31763010890d5dd46ffd
spellcry <sangiothebest@hotmail.com>
2022-07-25 15:54:58 +0200

    add mail check

--- mail/js/scripts.js
+++ mail/js/scripts.js
@@ -2,15 +2,19 @@ const mail = prompt('Inserisci la tua email');
 const emailValide = ['email1@test.com', 'email2@test.com', 'email3@test.com', 'email4@test.com', 'email5@test.com', 'email6@test.com', 'email7@test.com'];
 let trovato = false;
 
-for ( let i = 0; i < emailValide.length && !trovato; i++ ) {
-    emailCorrente = emailValide[i];
-    if ( mail === emailCorrente ) {
-        trovato = true;
+if ( mail != null ) {
+    for ( let i = 0; i < emailValide.length && !trovato; i++ ) {
+        emailCorrente = emailValide[i];
+        if ( mail === emailCorrente ) {
+            trovato = true;
+        }
     }
-}
-
-if ( trovato ) {
-    console.log(`L'email ${mail} può accedere`);
+    
+    if ( trovato ) {
+        console.log(`L'email ${mail} può accedere`);
+    } else {
+        console.log(`L'email ${mail} non può accedere`);
+    }
 } else {
-    console.log(`L'email ${mail} non può accedere`);
+    console.log('Devi inserire una mail');
 }
