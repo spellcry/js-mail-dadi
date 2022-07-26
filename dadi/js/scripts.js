0ea0edf94110e526282cba3a6da5ad550245a906
spellcry <sangiothebest@hotmail.com>
2022-07-26 10:25:44 +0200

    add js commenti

--- dadi/js/scripts.js
+++ dadi/js/scripts.js
@@ -1,8 +1,11 @@
 const min = Math.ceil(1);
 const max = Math.floor(6);
 
+// elemento in cui scrivere chi ha vinto
 const chiVinceElement = document.querySelector('.chi-vince');
 
+/* elementi in cui scrivere il punteggio
+   di computer e player */
 const playerResultElement = document.createElement('p');
 playerResultElement.classList.add('player__result');
 playerResultElement.classList.add('result');
@@ -10,11 +13,13 @@ const computerResultElement = document.createElement('p');
 computerResultElement.classList.add('computer__result');
 computerResultElement.classList.add('result');
 
+// aggiungo elementi con il punteggio nei rispettivi div
 const computerElement = document.querySelector('.results .computer');
 computerElement.append(computerResultElement);
 const playerElement = document.querySelector('.results .player');
 playerElement.append(playerResultElement);
 
+// event listener button gioca
 const giocaElement = document.addEventListener('click', function(){
     const computer = Math.floor(Math.random() * ( max - min + 1 ) + min);
     const player = Math.floor(Math.random() * ( max - min + 1 ) + min);
